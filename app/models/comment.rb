937da71010f39d5a9e5f8fc561d1ffaddf2fe94c
Chris Winslett <chris@mongohq.com>
2012-02-03 23:25:59 -0600

    Added some heroku settings.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,9 @@
-class Post
+class Comment
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :title, :type => String
-
-  has_one :message, autosave: true
-  accepts_nested_attributes_for :message
+  field :title, type: String
+  field :comment, type: String
 
   embeds_one :creator, as: :createable
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,11 @@ UmAkl::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => 'um-akl.com' }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"]
+  }
 
   # Enable threaded mode
   # config.threadsafe!
