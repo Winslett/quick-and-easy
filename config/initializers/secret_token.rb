51d347852997247c82d956f1c23026b9fab66829
Chris Winslett <chris@mongohq.com>
2012-02-03 21:05:11 -0600

    Added basics.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UmAkl::Application.config.secret_token = '04b302eb46256aa70f7db40a23d2935563a6360b49149d00f7a25acf8209d1bfa6cc44b7207f33f3f43a356d7c9e0bc5accf2ba1da481c57a29ed226153b8d82'
