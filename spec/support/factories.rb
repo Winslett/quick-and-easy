b954e0fad7f81579e2eaf65086cdbfbfe089da4d
Chris Winslett <chris@mongohq.com>
2012-02-10 18:03:43 -0600

    Fixed factories -- finally.

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -1,29 +1,35 @@
 FactoryGirl.define do
   factory :post do |p|
-    title Faker::Lorem.words
+    title {Faker::Lorem.words}
 
-    creator { |p| u = Factory(:user); Factory(:creator, :name => u.name, :email => u.email, :user_id => u.id) }
-    message { |p| Factory(:message, :title => p.title, :user_id => p.creator.user_id) }
+    creator do |p|
+      u = Factory(:user)
+      Factory.build(:creator, :name => u.name, :email => u.email, :user_id => u.id)
+    end
+
+    message do |p|
+      Factory.build(:message, :title => p.title, :user_id => p.creator.user_id)
+    end
   end
 
   factory :message do
-    title Faker::Lorem.words
-    body Faker::Lorem.paragraphs
+    title {Faker::Lorem.words}
+    body {Faker::Lorem.paragraphs}
 
     user { Factory(:user) }
   end
 
   factory :creator do
-    name Faker::Name.name
-    email Faker::Internet.email
+    name {Faker::Name.name}
+    email {Faker::Internet.email}
 
     user { |c| Factory :user, name: c.name, email: c.email }
   end
 
   factory :user do
-    name Faker::Name.name
-    email Faker::Internet.email
-    password Faker::Lorem.words(3).join(" ")
+    name {Faker::Name.name}
+    email {Faker::Internet.email}
+    password {Faker::Lorem.words(3).join(" ")}
     password_confirmation { |s| s.password }
   end
 end
